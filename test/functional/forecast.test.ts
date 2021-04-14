e07121c1e76577df5cb9a1e3d4c96cce77d8bbcb
Valdir Silva <v.souza.da.silva@avanade.com>
2021-04-13 21:09:58 -0300

    feat: create api and create firs test

--- test/functional/forecast.test.ts
+++ test/functional/forecast.test.ts
@@ -1,13 +1,9 @@
-import supertest from 'supertest';
-
 describe('Beach forecast functional tests', () => {
   
   it('should return a forecast with just a few times', async() => {
-    const { body, status } = await supertest(app).get('/forecast');
-    expect(status).toBe(200);
-    expect(body).toBe({
-      
-    });
-  });
+    const { body, status } = await global.testRequest.get('/forecast')
+    expect(status).toBe(200)
+    expect(body).toEqual([{}])
+  })
 
-});
+})
